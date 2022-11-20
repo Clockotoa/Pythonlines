4a24cae6d59f381dd4be9da5946034315612709a
Clockotoa <113152745+Clockotoa@users.noreply.github.com>
2022-11-20 03:13:44 -0500

    Added files via upload
    Script should be able to create 1, 3, 9, and 25 (with message) lines

---
+++ tryme4.py
@@ -0,0 +1,25 @@
+def new_line ():
+    print('.')
+
+def three_lines():
+    new_line()
+    new_line()
+    new_line()
+
+def nine_lines():
+    three_lines()
+    three_lines()
+    three_lines()
+
+def clear_screen():
+    print('Calling Clear Screen')
+    nine_lines()
+    nine_lines()
+    three_lines()
+    three_lines()
+    new_line()
+    
+new_line
+three_lines
+nine_lines
+clear_screen
